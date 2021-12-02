1df1d4a5f9073c03d906fd6c36de83e4a2a7ee5e
avbokov <bokov.itstep@gmail.com>
2021-12-02 21:21:15 +0700

    CW

--- Academy/main.cpp
+++ Academy/main.cpp
@@ -1,4 +1,5 @@
 ﻿#include<iostream>
+#include<fstream>
 #include<string>
 #include<ctime>
 #include<iomanip>
@@ -300,6 +301,18 @@ void main()
 		cout << *group[i] << endl;
 	}
 	cout << "\n--------------------------------\n";
+
+	std::ofstream fout("group.txt");
+
+	for (int i = 0; i < sizeof(group) / sizeof(Human*); i++)
+	{
+		//group[i]->print();
+		fout << typeid(*group[i]).name() << ":\t" << *group[i] << endl;
+	}
+
+	fout.close();
+
+	system("start notepad group.txt"); // открывается наш файл и не тормозит консоль
 
 	for (int i = 0; i < sizeof(group) / sizeof(group[0]); i++)
 	{
